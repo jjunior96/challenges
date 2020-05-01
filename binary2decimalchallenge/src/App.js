2e0617ff0bf4a1ac9a43cbc31c4d4bae03b31eca
Junior Alves <jjalves96@gmail.com>
2020-05-01 11:40:07 -0300

    feat: Add compoment

--- binary2decimalchallenge/src/App.js
+++ binary2decimalchallenge/src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import React, { useState } from 'react';
+
+import Form from './styles';
 
 function App() {
-  return <></>;
+  const [inputError, setInputError] = useState('');
+  const [binaryNumber, setBinaryNumber] = useState('');
+
+  function checkInput(e) {
+    e.preventDefault();
+    if (!/^[0-1]+$/.test(binaryNumber)) {
+      setInputError('Não é binário');
+      setBinaryNumber('');
+      return;
+    }
+    console.log('ok');
+  }
+
+  return (
+    <Form>
+      <label>
+        Binario:
+        <input
+          value={binaryNumber}
+          onChange={(e) => setBinaryNumber(e.target.value)}
+          placeholder="Digite um número binário"
+        ></input>
+      </label>
+      <input placeholder="Resultado"></input>
+      <button onClick={checkInput} type="submit">
+        Converter
+      </button>
+    </Form>
+  );
 }
 
 export default App;
